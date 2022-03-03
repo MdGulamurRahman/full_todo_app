331f4b62a20c816c3c8dee80581748198ac0203f
Abrarhusamfahim <fahimjahin321@gmail.com>
2022-03-03 20:09:38 +0600

    added another field

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ function displayToUi({name, priority, status, date, id}, index){
     <td id='priority'>${priority}</td>
     <td id='status'>${status}</td>
     <td id='date'>${date}</td>
-    <td>
+    <td id='action'>
         <button id="delete"><i class="fa-solid fa-trash"></i></button>
         <button id="check"><i class="fa-solid fa-square-check"></i></button>
         <button id="edit"><i class="fa-solid fa-pen-to-square"></i></button>
@@ -118,6 +118,104 @@ tbody.addEventListener("click", function(event){
                 
             }
         })
+    }else if(event.target.id == "edit"){
+        const table_tr = event.target.parentElement.parentElement;
+        const id = table_tr.dataset.id;
+        const table_td = table_tr.children;
+        //name
+        let nameTd;
+        let newNameField;
+        //priority
+        let priorityTd;
+        let prioritySelect;
+        //date
+        let dateTd;
+        let dateSelect;
+        //button
+        let buttonTd;
+        let preButtons;
+
+
+        [...table_td].forEach(td => {
+
+            if(td.id === "name"){
+                nameTd = td;
+                const preName = td.textContent;
+                td.innerText = "";
+                //create input
+                newNameField = document.createElement("input");
+                newNameField.type = "text";
+                newNameField.classList = "dynamic_input"
+                newNameField.value = preName;
+                td.appendChild(newNameField);
+            }else if(td.id === "priority"){
+                priorityTd = td;
+               const prePriority = td.textContent;
+               td.innerText = "";
+               prioritySelect = document.createElement("select");
+               prioritySelect.className = "dynamic_select"
+               prioritySelect.innerHTML = `
+                    <option disabled>Select One</option>
+                    <option value="high">High</option>
+                    <option value="medium">Medium</option>
+                    <option value="low">Low</option>
+               `;
+               if(prePriority == "high"){
+                prioritySelect.selectedIndex = 1;
+               }else if(prePriority == "medium"){
+                prioritySelect.selectedIndex = 2;
+               }else if(prePriority == "low"){
+                prioritySelect.selectedIndex = 3
+               }
+               td.appendChild(prioritySelect)
+            }else if(td.id === "date"){
+                dateTd = td;
+               const preDate = td.textContent;
+               td.innerText = "";
+               dateSelect = document.createElement("input");
+               dateSelect.type = "date";
+               dateSelect.classList = "dynamic_input"
+               dateSelect.value = preDate;
+               td.appendChild(dateSelect)
+            }else if(td.id === "action"){
+                buttonTd = td;
+                preButtons = td.innerHTML;
+                td.innerHTML = "";
+                const saveBtn =  document.createElement("button");
+                saveBtn.innerHTML = '<button"><i class="fa-solid fa-floppy-disk"></i></button>';
+                saveBtn.addEventListener("click", function(){
+                    //name
+                    const nameField = newNameField.value;
+                    nameTd.innerHTML = nameField;
+                    //priority
+                    const priorityField = prioritySelect.value;
+                    priorityTd.innerHTML = priorityField;
+                    //date 
+                    const dateField = dateSelect.value;
+                    dateTd.innerHTML = dateField;
+                    //button
+                    buttonTd.innerHTML = preButtons;
+                    //the area of localStorage
+                    let getTask = getTaskFromLocalStorage();
+                    getTask = getTask.filter(task =>{
+                        if(task.id == id){
+                            task.name = nameField;
+                            task.priority = priorityField;
+                            task.date = dateField;
+                            return task;
+                        }else{
+                            return task;
+                        }
+                    });
+                    setTaskLocalStorage(getTask)
+                });
+                td.appendChild(saveBtn)
+            }
+
+
+
+        })
+        
     }
     
 })
