03159539cb15e6b976df1a9cd4d385ef4a5801c6
Abrarhusamfahim <fahimjahin321@gmail.com>
2022-03-10 13:25:30 +0600

    i have done my js todo full project

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const filterField = getById("filter");
 const sortField = getById("sort");
 const by_date = getById("by_date");
 const date = getById("date");
+const bulkPriority = getById("bulk_priority");
+const bulkStatus = getById("bulk_status");
+const bulkEditInp = getById("edit_inp");
+const bulkEditSelect = getById("edit_sel");
 const today = new Date().toISOString().slice(0, 10);
 date.value = today;
 
@@ -182,6 +186,7 @@ form.addEventListener("submit", function(e){
     }
     
     this.reset()
+    getById("date").value = today;
 });
 
 window.onload = function(){
@@ -190,11 +195,189 @@ window.onload = function(){
         displayToUi(task, index + 1);
     })
 }
+// selected area
+let selectedTask = [];
+function selectFunc(e){
+    const tr = e.target.parentElement.parentElement;
+    const id = tr.dataset.id;
+    const isChacked = e.target.checked;
+    if(isChacked){
+        selectedTask.push(tr);
+        bulkActionHandler();
+    }else{
+        const index = selectedTask.findIndex((tr) => tr.dataset.id === id);
+        selectedTask.splice(index, 1);
+        bulkActionHandler();
+    }
+}
+//all selected
+getById("all_select").addEventListener("change", function(e){
+    const isChacked = e.target.checked;
+    const checkBoxes = document.querySelectorAll(".checkbox");
+    selectedTask = [];
+    if(isChacked){
+        ;[...checkBoxes].forEach(box => {
+            const tr = box.parentElement.parentElement;
+            selectedTask.push(tr)
+            box.checked = true;
+            bulkActionHandler();
+        })
+    }else{
+        ;[...checkBoxes].forEach(box => {
+            box.checked = false;
+            bulkActionHandler();
+        })
+    }
+    
+})
+//bulk action area 
+function bulkActionHandler(){
+    if(selectedTask.length){
+        getById("bulk_action").style.display = "flex"
+    }else{
+        getById("bulk_action").style.display = "none"
+    }
+}
+//dismiss area
+document.querySelector(".dismiss").addEventListener("click", function(e){
+    bulkPriority.selectedIndex = 0;
+    bulkStatus.selectedIndex = 0;
+    bulkEditInp.type = "text";
+    bulkEditInp.value = "";
+    bulkEditSelect.selectedIndex = 0;
+    const checkBoxes = document.querySelectorAll(".checkbox");
+    ;[...checkBoxes].forEach(box => {
+        box.checked = false;
+       
+    })
+    getById("all_select").checked = false;
+    selectedTask = [];
+    bulkActionHandler();
+})
+//delete selected task from localStorage
+getById("bulk_delete").addEventListener("click", function(e){
+    let getTask = getTaskFromLocalStorage();
+    selectedTask.forEach(tr =>{
+        const id = tr.dataset.id;
+        getTask = getTask.filter(task => task.id !== id)
+        tr.remove()
+    });
+    setTaskLocalStorage(getTask)
+});
+
+// bulkPriority area
+bulkPriority.addEventListener("change", function(e){
+    const selectPriority = this.value;
+    let getTask = getTaskFromLocalStorage();
+    selectedTask.forEach(tr =>{
+        const id = tr.dataset.id;
+        [...tr.children].forEach(td =>{
+            if(td.id == "priority"){
+                td.innerHTML = selectPriority;
+            }
+        })
+        getTask = getTask.filter(task =>{
+            if(task.id == id){
+                task.priority = selectPriority;
+                return task;
+            }else{
+                return task;
+            }
+        });
+    })
+    
+    setTaskLocalStorage(getTask)
+})
+
+//bulkStatus area
+bulkStatus.onchange = function(e){
+    const selectStatus = this.value;
+    let getTask = getTaskFromLocalStorage();
+    selectedTask.forEach(tr =>{
+        const id = tr.dataset.id;
+        [...tr.children].forEach(td =>{
+            if(td.id == "status"){
+                td.innerHTML = selectStatus;
+            }
+        })
+        getTask = getTask.filter(task =>{
+            if(task.id == id){
+                task.status = selectStatus;
+                return task;
+            }else{
+                return task;
+            }
+        });
+    })
+    
+    setTaskLocalStorage(getTask)
+}
+//bulk edit input or bulk edit select
+bulkEditSelect.onchange = function(e){
+    if(this.value == "name"){
+        bulkEditInp.type = "text";
+        bulkEditInp.value = ""
+    }else{
+        bulkEditInp.type = "date";
+        bulkEditInp.value = ""
+    }
+}
+
+bulkEditInp.oninput = function(e){
+    if(this.type == "text"){
+        const modifiedName = this.value;
+        let getTask = getTaskFromLocalStorage();
+        selectedTask.forEach(tr =>{
+            const id = tr.dataset.id;
+            [...tr.children].forEach(td =>{
+                if(td.id == "name"){
+                    td.innerHTML = modifiedName;
+                }
+            })
+            getTask = getTask.filter(task =>{
+                if(task.id == id){
+                    task.name = modifiedName;
+                    return task;
+                }else{
+                    return task;
+                }
+            });
+        })
+        
+        setTaskLocalStorage(getTask)
+    }else{
+        const modifiedDate = this.value;
+        let getTask = getTaskFromLocalStorage();
+        selectedTask.forEach(tr =>{
+            const id = tr.dataset.id;
+            [...tr.children].forEach(td =>{
+                if(td.id == "date"){
+                    td.innerHTML = modifiedDate;
+                }
+            })
+            getTask = getTask.filter(task =>{
+                if(task.id == id){
+                    task.date = modifiedDate;
+                    return task;
+                }else{
+                    return task;
+                }
+            });
+        })
+        
+        setTaskLocalStorage(getTask)
+    }
+}
 
 function displayToUi({name, priority, status, date, id}, index){
     const tr = document.createElement("tr");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "checkbox";
+    checkbox.addEventListener("change", selectFunc)
     tr.innerHTML = `
     <tr>
+    <td id="checkbox"></td>
     <td id='no'>${index}</td>
     <td id='name'>${name}</td>
     <td id='priority'>${priority}</td>
@@ -208,6 +391,7 @@ function displayToUi({name, priority, status, date, id}, index){
 </tr>
     `;
     tr.dataset.id = id;
+    tr.firstElementChild.appendChild(checkbox);
     tbody.appendChild(tr);
 }
 //=================================
